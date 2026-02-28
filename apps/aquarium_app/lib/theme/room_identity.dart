84ecfa7f4f9251777749cf9108480a4fae7026ca
Tiarnan larkin <larkintiarnan@googlemail.com>
2026-02-28 23:33:19 +0000

    fix(brand): last old teal reference in room_identity libraryTint

--- apps/aquarium_app/lib/theme/room_identity.dart
+++ apps/aquarium_app/lib/theme/room_identity.dart
@@ -25,7 +25,7 @@ class RoomIdentity {
 
   // Soft background tints for headers/cards
   static const Color livingRoomTint = Color(0x33E8A87C);
-  static const Color libraryTint = Color(0x335B9A8B);
+  static const Color libraryTint = Color(0x33B45309);
   static const Color labTint = Color(0x335AAF7A);
   static const Color workshopTint = Color(0x339C78FF);
   static const Color closetTint = Color(0x339CA3AF);
